44e385c9409c597d6c76734d752df87dc5a8a5e7
priyam.jain <priyam.jain@globallogic.com>
2022-03-10 19:20:24 +0530

    removed errors

--- src/Components/login/login.jsx
+++ src/Components/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Login(props) {
   const [username, setusername] = useState("");
